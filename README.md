a863291ad1409c83a5a28463d9f2251d03fb2198
Bryan Ricker <bricker88@gmail.com>
2012-11-08 17:14:58 -0800

    Update deploy file, remote haml, update readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-## Prison Letters
+## Prison Letters Project
 
+Originally hosted at <http://kpcc.org/specials/prison/letters>.
+
+### Dependancies
+* Sinatra
+
+### Deployment
+
+media:
+
+    cap deploy
+
+dev2:
+
+    cap staging deploy
--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :scm, :git
 set :repository,  "git@github.com:SCPR/prison_letters.git"
 set :scm_verbose, true
 set :deploy_via, :remote_cache
-set :deploy_to, "/web/projects/apps/prison"
+set :deploy_to, "/web/archive/apps/prison"
 
 set :user, "archive"
 set :use_sudo, false
@@ -16,12 +16,10 @@ set :group_writable, false
 set :branch, "master"
 set :rails_env, :production
 
-
 # --------------
 # Roles
-
-role :app,      "media.scpr.org"
-role :web,      "media.scpr.org"
+role :app, "media.scpr.org"
+role :web, "media.scpr.org"
 
 # Setup staging
 task :staging do
@@ -33,8 +31,9 @@ end
 after "deploy:restart", "deploy:cleanup"
 
 namespace :deploy do
-  task :start do ; end
-  task :stop do ; end
+  task :start do end
+  task :stop do end
+  task :migrate do end
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
