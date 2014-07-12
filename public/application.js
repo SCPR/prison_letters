b5510548fcd3b64096ef9a931f3a9bd21e4e65f1
Bryan Ricker <bricker88@gmail.com>
2014-07-12 01:35:52 -0700

    Cleanup

--- public/application.js
+++ public/application.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-    $("div.page").hover(
-          function () {
-            $("div."+$(this).attr('class').split(" ")[0]).addClass('highlightLetter');
-            $(this).addClass('highlightPage');
-          },
-          function () {
-            $("div."+$(this).attr('class').split(" ")[0]).removeClass('highlightLetter');
-            $(this).removeClass('highlightPage');
-          }
-        );
+  $("div.page").hover(
+    function () {
+      $("div."+$(this).attr('class').split(" ")[0]).addClass('highlightLetter');
+      $(this).addClass('highlightPage');
+    },
+    function () {
+      $("div."+$(this).attr('class').split(" ")[0]).removeClass('highlightLetter');
+      $(this).removeClass('highlightPage');
+    }
+  );
 });
